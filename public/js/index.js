6a80990b714255be10092f2ec2f61d46c2704cf9
Bang Nguyen Le Ly <bangjdev@gmail.com>
2021-03-14 20:14:03 +0100

    Update dogs pics

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
-const listBackgroundImages = ["back_1.jpeg", "back_2.jpg", "back_3.jpg", "back_4.jpg", "back_5.jpg", "back_6.jpg", "back_7.jpg"];
+const listBackgroundImages = ["back_1.jpeg", "back_2.jpg", "back_3.jpg", "back_4.jpg", "back_5.jpg", "back_6.jpg", "back_7.jpg", 
+                                "back_8.jpg", "back_9.jpg", "back_10.jpg", "back_11.jpg"];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -6,7 +7,7 @@ function getRandomInt(max) {
 
 const changeBackground = (listBackgroundImages) => {
     const id = getRandomInt(listBackgroundImages.length);
-    const imageUrl = "/public/assets/" + listBackgroundImages[id];
+    const imageUrl = `/public/assets/${listBackgroundImages[id]}`;
     document.getElementsByTagName("body")[0].style.background = `url("${imageUrl}") center center no-repeat`;
     document.getElementsByTagName("body")[0].style.backgroundSize = `cover`;
 }
